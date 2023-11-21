dc2c220b178dd7c5dd59d9a15ec5e5bb651ce05f
SilvaESilva099 <150605258+SilvaESilva099@users.noreply.github.com>
2023-11-21 11:17:04 -0300

    updating the game code
    I've redone the code with a more refined approach.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,57 +1,84 @@
 import random
+from enum import IntEnum
 
-player_options = ["rock", "paper", "scissors", "testicular torsion"]
-options = ["rock", "paper", "scissors"]
-options [0], [1], [2], [3]
-computer_action = random.choice(options)
+class Action(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+    Testicular_Torsion = 3
+
+class Actionputer(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+
+
+def get_user_selection():
+    choices = [f"{action.name}[{action.value}]" for action in Action]
+    choices_str = ", ".join(choices)
+    selection = int(input(f"Enter a choice ({choices_str}): "))
+    action = Action(selection)
+    return action
+
+def get_computer_selection():
+    selection = random.randint(0, len(Actionputer) - 1)
+    actionputer = Actionputer(selection)
+    return actionputer
+
+
 user_wins = 0
 computer_wins = 0
+ties = 0
 
-
-while True:
-
-    user_input = input("Choose an option: (Rock, Paper, Scissors, or press Q to quit:)\n").lower()
-    if user_input == "q":
-        break
-
-    if user_input not in player_options:
-        continue
-
-
-    print(f"\nYou chose {user_input}, computer chose {computer_action}.\n")
-
-    if user_input == computer_action:
-        print(f"Both players selected {user_input}. It's a tie!")
-    elif user_input == "rock":
-        if computer_action == "scissors":
+def determine_winner(user_action, computer_action):
+    global user_wins, computer_wins, ties
+    if user_action == computer_action:
+        print(f"Both players selected {user_action.name}. It's a tie!")
+        ties += 1
+    elif user_action == Action.Rock:
+        if computer_action == Action.Scissors:
             print("Rock smashes scissors! You win!")
             user_wins += 1
         else:
             print("Paper covers rock! You lose.")
             computer_wins += 1
-    elif user_input == "paper":
-        if computer_action == "rock":
+    elif user_action == Action.Paper:
+        if computer_action == Action.Rock:
             print("Paper covers rock! You win!")
             user_wins += 1
         else:
             print("Scissors cuts paper! You lose.")
             computer_wins += 1
-    elif user_input == "scissors":
-        if computer_action == "paper":
+    elif user_action == Action.Scissors:
+        if computer_action == Action.Paper:
             print("Scissors cuts paper! You win!")
             user_wins += 1
         else:
             print("Rock smashes scissors! You lose.")
             computer_wins += 1
-    elif user_input == "testicular torsion":
-        print ("TESTICULAR TORSION! YOU WIN!")
-        user_wins += 1
+    elif user_action == Action.Testicular_Torsion:
+        if computer_action != Action.Testicular_Torsion:
+            print("TESTICULAAAR TORSION! You win!")
+            user_wins += 1
+        else:
+            print("MUTUAL DESTRUCTION! NOBODY WINS!")
 
-    keep_playing = input("Do you wish to play again? (yes or y/no or n)\n")
-    if keep_playing != "yes":
+while True:
+    try:
+        user_action = get_user_selection()
+    except ValueError as e:
+        range_str = f"[0, {len(Action) - 1}]"
+        print(f"Invalid selection. Enter a value in range {range_str}")
+        continue
+
+    computer_action = get_computer_selection()
+    determine_winner(user_action, computer_action)
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
         break
-
-        
+    
 print ("You've won", user_wins, "times")
 print ("The computer won", computer_wins, "times")
+print ("You've tied", ties, "times")
 print ("See you next time!")
